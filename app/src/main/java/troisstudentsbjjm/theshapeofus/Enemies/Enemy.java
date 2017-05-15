f63a6c9085097b09964c04441ae6243317583cfb
Jeffherson Vega <jeffhersonlim@gmail.com>
2017-05-15 19:08:24 -0400

    Updated
    added Isdead setter and getter

--- app/src/main/java/troisstudentsbjjm/theshapeofus/Enemies/Enemy.java
+++ app/src/main/java/troisstudentsbjjm/theshapeofus/Enemies/Enemy.java
@@ -11,15 +11,22 @@ public abstract class Enemy {
     private float rotate;
     private int damage,
                 health;
+    private boolean isDead;
+
+    // Update here
 
     // Setter and Getter
     public void setVelocity(PointF velocity) { this.velocity = velocity;   }
     public void setRotate(float rotate) { this.rotate = rotate; }
-    
+    public void setDamage(int damage) { this.damage = damage;   }
+    public void setHealth(int health) { this.health = health;   }
+    public void setIsDead(boolean isDead) { this.isDead = isDead;   }
 
     public PointF getVelocity() { return velocity;  }
     public float getRotate() {  return rotate;  }
     public int getDamage() { return damage; }
+    public int getHealth() { return health; }
+    public boolean getIsDead() { return isDead; }
 
 
 }
