3c8d685cc16bad8f1384e99df607b34991e4a51d
Braedon Alexandre Jolie <hotshot-braedon@hotmail.com>
2017-05-16 19:42:27 -0400

    Input started

--- app/src/main/java/troisstudentsbjjm/theshapeofus/Input/InputController.java
+++ app/src/main/java/troisstudentsbjjm/theshapeofus/Input/InputController.java
@@ -2,6 +2,7 @@ package troisstudentsbjjm.theshapeofus.Input;
 
 /**
  * Created by mrber on 2017-05-15.
+ * Edited by Braedon Jolie 2017-05-16.
  */
 
 //We can implement a joystick controller class down the line
@@ -10,4 +11,28 @@ public class InputController {
 
     public double mHorizontalFactor;
     public double mVerticalFactor;
+
+    public boolean isTapping;
+    public boolean upgradeTap;
+
+
+    public void onStart() {
+
+    }
+
+    public void onStop() {
+
+    }
+
+    public void onPause() {
+
+    }
+
+    public void onResume() {
+
+    }
+
+    public void onPreUpdate() {
+
+    }
 }
---
+++ app/src/main/java/troisstudentsbjjm/theshapeofus/Input/LevelInterface.java
@@ -0,0 +1,37 @@
+package troisstudentsbjjm.theshapeofus.Input;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+import troisstudentsbjjm.theshapeofus.R;
+
+/**
+ * Created by Braedon Jolie on 2017-05-16.
+ */
+
+public class LevelInterface extends InputController {
+    // Level Interface will have all the elements needed for the player to interface things in the level
+    // Like panning/scaling the screen, interacting with defenses, upgrading, etc...
+
+    LevelInterface(View view) {
+        view.findViewById(R.id.upgrade_button).setOnTouchListener(UpgradeButton);
+    }
+
+    private class UpgradeButton implements View.OnTouchListener {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            int action = event.getActionMasked();
+
+            if(action == MotionEvent.ACTION_DOWN) {
+                if(upgradeTap == true) {
+                    upgradeTap = false;
+                } else if (upgradeTap == false) {
+                    upgradeTap = true;
+                }
+            } else if(action == MotionEvent.ACTION_UP) {
+
+            }
+            return true;
+        }
+    }
+}
