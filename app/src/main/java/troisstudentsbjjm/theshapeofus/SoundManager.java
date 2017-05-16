80215ff7e1b750ed80f26833ddec2b6eac04ee41
Jeffherson Vega <jeffhersonlim@gmail.com>
2017-05-16 19:05:02 -0400

    SoundManager

---
+++ app/src/main/java/troisstudentsbjjm/theshapeofus/SoundManager.java
@@ -0,0 +1,30 @@
+package troisstudentsbjjm.theshapeofus;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.util.Log;
+
+import java.io.IOException;
+
+/**
+ * Created by Jeffherson on 2017-05-16.
+ */
+
+public class SoundManager {
+    private SoundPool soundPool;
+    // Sound Assets Here
+
+//    public void loadSound(Context context) {
+//        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+//        try {
+//            AssetManager assetManager = context.getAssets();
+//            AssetFileDescriptor descriptor;
+//            // Put the Sound
+//        }   catch (IOException e) {
+//            Log.e("error", "Failed to Load Sound Files");
+//        }
+//    }
+}
