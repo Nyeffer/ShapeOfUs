48699903170ad2c0f9659527b57e2c9c6be5d864
Jason <mrberry1990@gmail.com>
2017-05-23 03:11:23 -0400

    TowerMenuClass update
    more functionality

--- app/src/main/java/troisstudentsbjjm/theshapeofus/Input/TowerMenu.java
+++ app/src/main/java/troisstudentsbjjm/theshapeofus/Input/TowerMenu.java
@@ -1,5 +1,8 @@
 package troisstudentsbjjm.theshapeofus.Input;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
 import troisstudentsbjjm.theshapeofus.Primatives.Circle;
 import troisstudentsbjjm.theshapeofus.Primatives.GameObject;
 import troisstudentsbjjm.theshapeofus.Primatives.Square;
@@ -31,6 +34,50 @@ public class TowerMenu extends GameObject {
         location.set(0,0);
         isActive = false;
 
-        
+        S_button = new Triangle();
+        C_button = new Triangle();
+        T_button = new Triangle();
+
+    }
+
+
+    public void moveAndDisplay(int x, int y){
+        location.set(x,y);
+        center.center.set(location.x,location.y);
+        center.size = (float) (size*0.25);
+        setButtons();
+        setIcons();
+        isActive = true;
+    }
+
+
+    private void setButtons(){
+        S_button.A.set(location.x, location.y);
+        S_button.B.set(location.x + size, (float) (location.y + 0.5*size));
+        S_button.C.set(S_button.B.x, S_button.B.y - size);
+
+        C_button.A.set(S_button.A.x, S_button.A.y);
+        C_button.B.set(C_button.A.x - size, S_button.B.y);
+        C_button.C.set(C_button.B.x, S_button.C.y);
+
+        T_button.A.set(S_button.A.x, S_button.A.y);
+        T_button.B.set((float) (T_button.A.x - 0.5*size), T_button.A.y - size);
+        T_button.C.set(T_button.B.x + size, T_button.B.y);
+    }
+
+
+    private void setIcons(){
+
+    }
+
+
+    public void draw(Canvas canvas, Paint paint){
+        if (isActive){
+            //draw Buttons First
+
+            //then draw center
+
+            //then draw icons
+        }
     }
 }
